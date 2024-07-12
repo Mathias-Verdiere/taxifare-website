f189b72cfce8c13eee8597ba8d4f49377bfe0838
Mathias Verdiere <mathias.verdiere@youkaidi.com>
2024-07-12 15:01:45 +0200

    V0

--- app.py
+++ app.py
@@ -1,53 +1,70 @@
 import streamlit as st
+import base64
+import datetime
+import requests
 
-image_path = 'img/taxi.jpeg'
+img_taxi_path = 'img/taxi.jpeg'
+url_predict = 'https://taxifare.lewagon.ai/predict'
 
-st.write(f'<a href="{image_path}"></a>', unsafe_allow_html=True)
+
+def get_location(address:str)->tuple:
+    url = "https://nominatim.openstreetmap.org/search"
+    params= {"q":address, "format":"json"}
+    response = requests.get(url, params = params)
+    print(response.content)
+    return response.json()[0]["lat"], response.json()[0]["lon"]
 
 
-'''
-# TaxiFareModel front
-'''
+#latlon = get_location("13, Rue de la glacière, 75013, paris, france")
+#print(latlon)
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
+def load_image(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+    encoded = base64.b64encode(data).decode()
+    return encoded
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+def image_tag(path):
+    encoded = load_image(path)
+    tag = f'<img src="data:image/png;base64,{encoded}" style="width:150px">'
+    return tag
 
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
+st.title('Taxifare')
+st.write(image_tag(img_taxi_path), unsafe_allow_html=True)
+st.title('The best fare estimator in NY')
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
 
-url = 'https://taxifare.lewagon.ai/predict'
+passenger_count = st.slider('Number of passenger', 1, 8, 1)
 
-if url == 'https://taxifare.lewagon.ai/predict':
+st.markdown('## Pick-up')
+pickup_date = st.date_input( "Date",  datetime.datetime.now())
+pickup_time = st.time_input( "Time",  datetime.datetime.now())
+pickup_lat = st.number_input("Lat", key="pickup_lat")
+pickup_lon = st.number_input("Lon", key="pickup_lon")
 
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+st.markdown('## Drop-off')
+dropoff_lat = st.number_input("Lat", key="dropoff_lat")
+dropoff_lon = st.number_input("Lon", key="dropoff_lon")
 
-'''
 
-2. Let's build a dictionary containing the parameters for our API...
+pickup_dt =pickup_date.strftime("%Y-%m-%d")  + " " +  pickup_time.strftime("%H:%M:%S")
 
-3. Let's call our API using the `requests` package...
 
-4. Let's retrieve the prediction from the **JSON** returned by the API...
 
-## Finally, we can display the prediction to the user
-'''
+params = {'pickup_datetime':pickup_dt,
+'pickup_longitude':pickup_lon,
+'pickup_latitude':pickup_lat,
+'dropoff_longitude':dropoff_lon,
+'dropoff_latitude':dropoff_lat,
+'passenger_count': passenger_count}
+
+
+if st.button("Calculer"):
+    response = requests.get(url_predict, params=params).json()
+
+
+    r = round(response['fare'], 2)
+    st.markdown(f"## Prix : ${r}")
